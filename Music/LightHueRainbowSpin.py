0a46a49fa794293a10c4bcd2fa64e12f45b5bf66
Alastair <alastair.hawkes@gmail.com>
2017-03-05 22:12:32 +0000

    test

--- Music/LightHueRainbowSpin.py
+++ Music/LightHueRainbowSpin.py
@@ -64,13 +64,13 @@ progress = 0
 def wheel(pos):
         """Generate rainbow colors across 0-255 positions."""
         if pos < 85:
-                return Color(pos * 3, 255 - pos * 3, 0)
+                return Color(0, 0, 0)
         elif pos < 170:
                 pos -= 85
-                return Color(255 - pos * 3, 0, pos * 3)
+                return Color(0, 0, pos * 3)
         else:
                 pos -= 170
-                return Color(0, pos * 3, 255 - pos * 3)
+                return Color(0, 0, 255 - pos * 3)
 
 while stream.is_active():
     #print datasamp
